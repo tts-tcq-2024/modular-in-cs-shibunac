09565d5f076fa3b7d2557ba86f95eb0f59ea0d46
shibunac <shibunachedakal@gmail.com>
2024-10-06 12:37:53 +0530

    Create ColorLibrary.cs

---
+++ ColorLibrary.cs
@@ -0,0 +1,18 @@
+namespace 
+{
+    /// <summary>
+    /// Repository that holds the major and minor color mappings.
+    /// </summary>
+    public static class ColorLibrary
+    {
+        public static readonly List<string> MajorColors = new List<string>
+        {
+            "White", "Red", "Black", "Yellow", "Violet"
+        };
+
+        public static readonly List<string> MinorColors = new List<string>
+        {
+            "Blue", "Orange", "Green", "Brown", "SlateGray"
+        };
+    }
+}
