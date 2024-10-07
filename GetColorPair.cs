7f7b0d3b5d9344cb85f4d8c790b1ec276fbb666f
shibunac <shibunachedakal@gmail.com>
2024-10-07 19:09:23 +0530

    Create GetColorPair.cs

---
+++ GetColorPair.cs
@@ -0,0 +1,19 @@
+/// <summary>
+    /// Represents a pair of colors.
+    /// </summary>
+    public class ColorPair
+    {
+        public Color MajorColor { get; }
+        public Color MinorColor { get; }
+
+        public ColorPair(Color majorColor, Color minorColor)
+        {
+            MajorColor = majorColor;
+            MinorColor = minorColor;
+        }
+
+        public override string ToString()
+        {
+            return $"MajorColor: {MajorColor.Name}, MinorColor: {MinorColor.Name}";
+        }
+    }
