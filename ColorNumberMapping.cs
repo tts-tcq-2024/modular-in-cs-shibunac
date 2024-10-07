1ae669277a923852f8d62f6f2b63dabd7561841f
shibunac <shibunachedakal@gmail.com>
2024-10-07 19:28:14 +0530

    Create ColorNumberMapping.cs

---
+++ ColorNumberMapping.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace TeleCom
+{
+  /// <summary>
+  /// Handles mapping between pair numbers and color pairs.
+  /// </summary>
+  public class ColorMapper
+  {
+    private readonly int _majorSize;
+    private readonly int _minorSize;
+
+    public ColorMapper()
+    {
+      _majorSize = ColorRepository.MajorColors.Count;
+      _minorSize = ColorRepository.MinorColors.Count;
+    }
+
+    /// <summary>
+    /// Given a pair number, returns the corresponding ColorPair.
+    /// </summary>
+    /// <param name="pairNumber">Pair number (1-based index).</param>
+    /// <returns>ColorPair corresponding to the pair number.</returns>
+    public ColorPair GetColorFromPairNumber(int pairNumber)
+    {
+      if (pairNumber < 1 || pairNumber > _majorSize * _minorSize)
+      {
+        throw new ArgumentOutOfRangeException(nameof(pairNumber),
+            $"Pair number must be between 1 and {_majorSize * _minorSize}.");
+      }
+
+      int zeroBasedPairNumber = pairNumber - 1;
+      int majorIndex = zeroBasedPairNumber / _minorSize;
+      int minorIndex = zeroBasedPairNumber % _minorSize;
+
+      string majorColor = ColorRepository.MajorColors[majorIndex];
+      string minorColor = ColorRepository.MinorColors[minorIndex];
+
+      return new ColorPair(majorColor, minorColor);
+    }
+
+    /// <summary>
+    /// Given a ColorPair, returns the corresponding pair number.
+    /// </summary>
+    /// <param name="pair">ColorPair to be mapped.</param>
+    /// <returns>Pair number corresponding to the ColorPair.</returns>
+    public int GetPairNumberFromColor(ColorPair pair)
+    {
+      if (pair == null)
+        throw new ArgumentNullException(nameof(pair));
+
+      int majorIndex = ColorRepository.MajorColors.IndexOf(pair.MajorColor);
+      int minorIndex = ColorRepository.MinorColors.IndexOf(pair.MinorColor);
+
+      if (majorIndex == -1 || minorIndex == -1)
+      {
+        throw new ArgumentException($"Unknown Colors: {pair}");
+      }
+
+      return (majorIndex * _minorSize) + (minorIndex + 1);
+    }
+  }
+}
