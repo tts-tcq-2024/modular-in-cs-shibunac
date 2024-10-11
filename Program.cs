e0c3719af1c324b04734644921379bc59d8376a2
shibunac <shibunachedakal@gmail.com>
2024-10-11 16:22:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,10 +4,9 @@ namespace ColorCoding
 {
   public static void Main(string[] args)
   {
-    var mapper = new ColorCodeMapper();
     var colorCodeTest = new ColorCodeTester();
-    colorCodeTest.RunPairNumberToColorTests(mapper);
-    colorCodeTest.RunColorToPairNumberTests(mapper);
+    colorCodeTest.RunPairNumberToColorTests();
+    colorCodeTest.RunColorToPairNumberTests();
     
   }
 }
